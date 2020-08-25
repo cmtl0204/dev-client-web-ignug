23e235ce5a2a5fba78dff2c2783f4bf17df3174e
cmtl0204 <cesar.tamayo0204@gmail.com>
2020-08-25 15:07:18 -0500

    cambios jobboard

--- src/app/pages/job-board/hoja-vida/app.hoja-vida.component.ts
+++ src/app/pages/job-board/hoja-vida/app.hoja-vida.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {ConfirmationService, MessageService} from 'primeng/api';
+import {Professional} from '../../../models/job-board/models.index';
+import {JobBoardServiceService} from '../../../services/job-board/job-board-service.service';
+import {IgnugServiceService} from '../../../services/ignug/ignug-service.service';
 
 @Component({
     selector: 'app-hoja-vida',
@@ -9,7 +12,30 @@ import {ConfirmationService, MessageService} from 'primeng/api';
     providers: [MessageService, ConfirmationService]
 })
 export class AppHojaVidaComponent implements OnInit {
+    professional: Professional;
+    professionals: Array<Professional>;
+    professionalSelected: Professional;
+
+    constructor(private jobBoardService: JobBoardServiceService,
+                private ignugService: IgnugServiceService) {
+        this.professional = new Professional();
+        this.professionalSelected = new Professional();
+        this.professionals = new Array<Professional>();
+    }
     ngOnInit(): void {
+
     }
 
+    getProfessionals(): void {
+        this.jobBoardService.get('professionals').subscribe(
+            response => {
+                this.professionals = response['data']['attributes'];
+                console.log(this.professionals);
+            },
+            error => {
+                console.log('error');
+                console.log(error);
+            }
+        );
+    }
 }

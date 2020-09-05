9f54474f6cf9f592569b0c9a953bc616fda4c397
cmtl0204 <cesar.tamayo0204@gmail.com>
2020-09-05 12:43:45 -0500

    cambios

---
+++ src/app/pages/web/administrator/app.menu/app.menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app.menu',
+  templateUrl: './app.menu.component.html',
+  styleUrls: ['./app.menu.component.css']
+})
+export class App.MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

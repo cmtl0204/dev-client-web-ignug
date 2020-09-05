5f76765de11411b4be30292a079c8aa9dba3be9e
cmtl0204 <cesar.tamayo0204@gmail.com>
2020-09-05 12:45:26 -0500

    cambios

--- src/app/pages/web/administrator/administrator.module.ts
+++ src/app/pages/web/administrator/administrator.module.ts
@@ -45,6 +45,7 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { ToastModule } from 'primeng/toast';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { MegaMenuModule } from 'primeng/megamenu';
+import { App } from './app-administrator/app.administrator.component';
 
 
 // Mis componentes
@@ -95,7 +96,7 @@ import { MegaMenuModule } from 'primeng/megamenu';
     ConfirmDialogModule,
     MegaMenuModule,
   ],
-  declarations: [],
+  declarations: [AppAdministratorComponent],
   providers: [DialogService],
 })
 export class AdministratorModule {}
